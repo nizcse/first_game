e2e1a9f3a04f70825660987a3632f3132d91e0e8
nizcse <66297072+nizcse@users.noreply.github.com>
2024-10-11 08:27:25 +0530

    feat: added collision detection to ball

--- script.js
+++ script.js
@@ -4,69 +4,114 @@
 
 const canvas = document.getElementById("canvas");
 
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
+const WIDTH = document.body.clientWidth;
+const HEIGHT = document.body.clientHeight;
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+canvas.offsetWidth = document.body.clientWidth;
+canvas.offsetHeight = document.body.clientHeight;
 const BALL_RADIUS = 7.5;
 const LEFTPAD = 30;
 const RIGHTPAD = 30;
 
-/**
- * @type {CanvasRenderingContext2D}
- */
-const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
 let sliderLeftWidth = 10;
 let sliderLeftHeight = 50;
 let sliderRightWidth = 10;
 let sliderRightHeight = 50;
 
-ctx.fillStyle = "#FF0000";
+let ballMomentSpeed = {
+    x: 7,
+    y: 2
+};
+
+let ballPosition = {
+    x: WIDTH * 0.5,
+    y: HEIGHT * 0.5
+};
+
+let leftBarPosition = {
+    x: LEFTPAD,
+    y: HEIGHT * 0.5 - sliderLeftHeight * 0.5,
+}
+
+let rightBarPosition = {
+    x: WIDTH - RIGHTPAD - sliderRightWidth,
+    y: HEIGHT * 0.5 - sliderRightHeight * 0.5,
+}
+
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+const ctx = canvas.getContext("2d");
+
 // ctx.fillRect(30, 375, 40, 425)
-ctx.fillRect(
-  LEFTPAD,
-  HEIGHT * 0.5 - sliderLeftHeight * 0.5,
-  sliderLeftWidth,
-  sliderLeftHeight
-);
+// ctx.fillRect(
+//   LEFTPAD,
+//   HEIGHT * 0.5 - sliderLeftHeight * 0.5,
+//   sliderLeftWidth,
+//   sliderLeftHeight
+// );
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(
-  WIDTH - RIGHTPAD - sliderRightWidth,
-  HEIGHT * 0.5 - sliderRightHeight * 0.5,
-  sliderRightWidth,
-  sliderRightHeight
-);
+const renderPaddle = (x, y) => {
+    ctx.fillRect(
+        x,
+        y,
+        sliderRightWidth,
+        sliderRightHeight
+    );
 
-ctx.beginPath();
-ctx.strokeStyle = "#00FF00";
-ctx.arc(WIDTH * 0.5, HEIGHT * 0.5, BALL_RADIUS, 0, 2 * Math.PI);
-ctx.fillStyle = "#00FF00";
-ctx.fill();
-ctx.closePath();
+}
+
+const renderBall = (x, y) => {
+    ctx.beginPath();
+    ctx.strokeStyle = '#00FF00';
+    ctx.arc(x, y, BALL_RADIUS, 0, 2 * Math.PI);
+    ctx.fillStyle = '#00FF00'
+    ctx.fill()
+    ctx.closePath();
+}
 
 /**
  * @param {Date} initialTime - preview function call date
  *
  * @returns {void}
  */
-let showFps = false;
+let showFps = true;
 const animation = (initialTime) => {
-  const endTime = new Date();
-  const nextFrameTimeDifference = endTime - initialTime;
-  console.log(nextFrameTimeDifference);
-  const startTime = new Date();
-  console.log("new frame dropped");
-  if (showFps)
-    ctx.strokeText(Math.floor(1000 / (nextFrameTimeDifference || 1)), 10, 10);
+    const endTime = new Date();
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = '#000000'
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    const nextFrameTimeDifference = endTime - initialTime;
+    console.log(nextFrameTimeDifference);
+    const startTime = new Date();
+    if (ballPosition.x + BALL_RADIUS < 2 * BALL_RADIUS) {
+        ballMomentSpeed.x = ballMomentSpeed.x * -1
+    }
+    if (ballPosition.x + BALL_RADIUS > WIDTH) {
+        ballMomentSpeed.x = ballMomentSpeed.x * -1
+    }
+    ballPosition.x = ballPosition.x + ballMomentSpeed.x;
 
-  window.requestAnimationFrame(() =>
-    setTimeout(() => {
-      if (showFps) ctx.clearRect(0, 0, 120, 30);
-      animation(startTime);
-    }, 59 / 1000)
-  );
-};
+    ctx.fillStyle = "#FF0000";
+    renderBall(ballPosition.x, HEIGHT * 0.5);
+
+    ctx.fillStyle = "#FF0000";
+    renderPaddle(leftBarPosition.x, leftBarPosition.y);
+    renderPaddle(rightBarPosition.x, rightBarPosition.y);
+
+    if (showFps) {
+        ctx.font = "20px monospace";
+        ctx.fillText(Math.floor(1000 / (nextFrameTimeDifference || 1)), 10, 20);
+    }
+    window.requestAnimationFrame(() =>
+        setTimeout(() => {
+          if (showFps) ctx.clearRect(0, 0, 120, 30);
+          animation(startTime);
+        }, 59 / 1000)
+      );
+}
 
 animation(new Date());
