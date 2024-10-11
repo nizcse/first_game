99d7ba28a64984777872f33d57b2ff30dae67898
nizcse <66297072+nizcse@users.noreply.github.com>
2024-10-11 09:00:39 +0530

    feat: partially added movement to paddle

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
  * @type {HTMLCanvasElement}
  * */
 
+
+/**
+ * TODOS:
+ * Movement of Paddle on keyboard Event
+ * Collision of Slider on ball
+ * Score
+ * Game Over
+ * Pause
+ */
+
 const canvas = document.getElementById("canvas");
 
 const WIDTH = document.body.clientWidth;
 const HEIGHT = document.body.clientHeight;
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
-canvas.offsetWidth = document.body.clientWidth;
-canvas.offsetHeight = document.body.clientHeight;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+canvas.offsetWidth = WIDTH;
+canvas.offsetHeight = HEIGHT;
 const BALL_RADIUS = 7.5;
 const LEFTPAD = 30;
 const RIGHTPAD = 30;
@@ -39,7 +49,22 @@ let leftBarPosition = {
 let rightBarPosition = {
     x: WIDTH - RIGHTPAD - sliderRightWidth,
     y: HEIGHT * 0.5 - sliderRightHeight * 0.5,
+};
+
+let sliderMovementSpeed = {
+    up: 5,
+    down: 5,
 }
+
+let moveRightSlider = {
+    up: false,
+    down: true,
+};
+
+let moveLeftSlider = {
+    up: false,
+    down: true,
+};
 
 /**
  * @type {CanvasRenderingContext2D}
@@ -79,13 +104,56 @@ const renderBall = (x, y) => {
  * @returns {void}
  */
 let showFps = true;
+
+const rightPaddleMovement = (event) => {
+    console.log(event.key === 'ArrowUp');
+    if (event.key === 'ArrowUp') {
+        moveRightSlider.down = true;
+        moveRightSlider.up = false;
+    } else if (event.key === 'ArrowDown') {
+        moveRightSlider.down = true;
+        moveRightSlider.up = false;
+    } else {
+        moveRightSlider.down = false;
+        moveRightSlider.up = false;
+    }
+}
+
+const leftPaddleMovement = (event) => {
+    if (event.key === 'W') {
+        moveRightSlider.down = true;
+        moveRightSlider.up = false;
+    } else if (event.key === 'S') {
+        moveRightSlider.down = true;
+        moveRightSlider.up = false;
+    } else {
+        moveRightSlider.down = false;
+        moveRightSlider.up = false;
+    }
+}
+
+const rightPaddleStop = () => {
+    moveRightSlider.down = false;
+    moveRightSlider.up = false;
+}
+
+const leftPaddleStop = () => {
+    moveLeftSlider.down = false;
+    moveLeftSlider.up = false;
+}
+
+document.body.addEventListener('keypress', rightPaddleMovement);
+document.body.addEventListener('keypress', leftPaddleMovement);
+document.body.addEventListener('keyup', rightPaddleStop);
+document.body.addEventListener('keyup', leftPaddleStop);
+
+
 const animation = (initialTime) => {
     const endTime = new Date();
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     ctx.fillStyle = '#000000'
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     const nextFrameTimeDifference = endTime - initialTime;
-    console.log(nextFrameTimeDifference);
     const startTime = new Date();
     if (ballPosition.x + BALL_RADIUS < 2 * BALL_RADIUS) {
         ballMomentSpeed.x = ballMomentSpeed.x * -1
@@ -93,14 +161,42 @@ const animation = (initialTime) => {
     if (ballPosition.x + BALL_RADIUS > WIDTH) {
         ballMomentSpeed.x = ballMomentSpeed.x * -1
     }
+    if (ballPosition.y + BALL_RADIUS > HEIGHT) {
+        ballMomentSpeed.y = ballMomentSpeed.y * -1
+    }
+    if (ballPosition.y + BALL_RADIUS < 2 * BALL_RADIUS) {
+        ballMomentSpeed.y = ballMomentSpeed.y * -1
+    }
     ballPosition.x = ballPosition.x + ballMomentSpeed.x;
+    ballPosition.y = ballPosition.y + ballMomentSpeed.y;
 
     ctx.fillStyle = "#FF0000";
-    renderBall(ballPosition.x, HEIGHT * 0.5);
+    renderBall(ballPosition.x, ballPosition.y);
 
     ctx.fillStyle = "#FF0000";
     renderPaddle(leftBarPosition.x, leftBarPosition.y);
-    renderPaddle(rightBarPosition.x, rightBarPosition.y);
+    if (moveRightSlider.up) {
+        rightBarPosition.y = rightBarPosition.y - sliderMovementSpeed.up;
+        renderPaddle(rightBarPosition.x, rightBarPosition.y);
+    } else if (moveRightSlider.down) {
+        rightBarPosition.y = rightBarPosition.y + sliderMovementSpeed.down;
+        renderPaddle(rightBarPosition.x, rightBarPosition.y);
+    } else {
+        renderPaddle(rightBarPosition.x, rightBarPosition.y);
+    }
+
+    // Left slider
+    if (moveLeftSlider.up) {
+        leftBarPosition.y = leftBarPosition.y - sliderMovementSpeed.up;
+        leftPaddleMovement(leftBarPosition.x, leftBarPosition.y);
+    } else if (moveLeftSlider.down) {
+        leftBarPosition.y = leftBarPosition.y + sliderMovementSpeed.down;
+        leftPaddleMovement(leftBarPosition.x, leftBarPosition.y);
+    } else {
+        leftPaddleMovement(leftBarPosition.x, leftBarPosition.y);
+    }
+
+    console.log(moveLeftSlider);
 
     if (showFps) {
         ctx.font = "20px monospace";
@@ -108,10 +204,10 @@ const animation = (initialTime) => {
     }
     window.requestAnimationFrame(() =>
         setTimeout(() => {
-          if (showFps) ctx.clearRect(0, 0, 120, 30);
-          animation(startTime);
+            if (showFps) ctx.clearRect(0, 0, 120, 30);
+            animation(startTime);
         }, 59 / 1000)
-      );
+    );
 }
 
 animation(new Date());
