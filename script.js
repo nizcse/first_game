3029264cc105f8503d96c3bedd81290a6377251b
kushmakhija <kushmakhija4@gmail.com>
2024-10-11 06:25:47 +0530

    Feat!: right pad

--- script.js
+++ script.js
@@ -2,24 +2,37 @@
  * @type {HTMLCanvasElement}
  * */
 
-const canvas = document.getElementById('canvas');
-const WIDTH = 400;
-const HEIGHT = 400;
-
-canvas.width = WIDTH;
-canvas.HEIGHT = HEIGHT;
+const canvas = document.getElementById("canvas");
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
 
 /**
  * @type {CanvasRenderingContext2D}
  */
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-const sliderWidth = 10;
-const sliderHeight = 50;
+let sliderLeftWidth = 10;
+let sliderLeftHeight = 50;
+let sliderRightWidth = 10;
+let sliderRightHeight = 50;
 
-const LEFTPAD = 30
-ctx.fillStyle = '#FF0000'
-// ctx.fillRect(30, 375, sliderWidth, sliderHeight)
-ctx.fillRect(LEFTPAD, HEIGHT * 0.5 - sliderHeight * 0.5, sliderWidth, sliderHeight * 0.5)
+const LEFTPAD = 30;
+ctx.fillStyle = "#FF0000";
+// ctx.fillRect(30, 375, 40, 425)
+ctx.fillRect(
+  LEFTPAD,
+  HEIGHT * 0.5 - sliderLeftHeight * 0.5,
+  sliderLeftWidth,
+  sliderLeftHeight
+);
+
+const RIGHTPAD = 30;
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(
+  WIDTH - RIGHTPAD - sliderRightWidth,
+  HEIGHT * 0.5 - sliderRightHeight * 0.5,
+  sliderRightWidth,
+  sliderRightHeight
+);
